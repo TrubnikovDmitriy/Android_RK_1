e01ac84be3a1b9a061575373edb1e13284155e28
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2018-03-07 22:27:42 +0300

    make nice

--- app/src/main/java/com/example/dmitriy/rk_1_android/ArticleFragment.java
+++ app/src/main/java/com/example/dmitriy/rk_1_android/ArticleFragment.java
@@ -7,10 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
-import java.util.Date;
 
 import ru.mail.park.articlelistlib.Article;
 
--- app/src/main/java/com/example/dmitriy/rk_1_android/MainActivity.java
+++ app/src/main/java/com/example/dmitriy/rk_1_android/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.Date;
-
 import ru.mail.park.articlelistlib.Article;
 import ru.mail.park.articlelistlib.ArticleListFragment;
 import ru.mail.park.articlelistlib.OnArticleClickListener;
